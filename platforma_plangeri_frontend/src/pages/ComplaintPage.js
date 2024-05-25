2488c98cba30c275d225ecea9a0e55ca1a938bb3
Alex Blaga <cosmin.blaga@student>
2024-05-25 09:38:07 +0300

    modificat css-uri pentru culori mai vizibile

--- platforma_plangeri_frontend/src/pages/ComplaintPage.js
+++ platforma_plangeri_frontend/src/pages/ComplaintPage.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
+import './ComplaintPage.css';
 
 function ComplaintPage() {
   const { id } = useParams();
